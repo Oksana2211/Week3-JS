8c65e71859104d28567a7ee95271b9ca2f1af5af
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-03-31 12:40:48 +0300

    add function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//корзина-скидка
 const namber1 = document.getElementById("namber1").innerHTML;
 const namber2 = document.getElementById("namber2").innerHTML;
 const namber3 = document.getElementById("namber3").innerHTML;
@@ -9,5 +10,14 @@ paid.textContent = sum;
 function clickDiscount() {
 paid.textContent = sum-((sum*20)/100);
 }
+//
 
+//вычисление квадрата
+const input1 = document.getElementsByClassName('input-mrg')[0];
+const input2 = document.getElementsByClassName('input-mrg')[1];
 
+function clicCalculation() {
+  let num1 = input1.value;
+  input2.value = num1**2;
+}
+//
